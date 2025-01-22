2ded1776157de078d891868461180319b191f5bc
Aryan Joshi <aryanjoshi014@gmail.com>
2025-01-22 21:35:29 +0530

    project structure ready, venv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aryan Joshi",
+    author_email="aryanjoshi014@gmail.com",
+    packages=find_packages(),
+)
